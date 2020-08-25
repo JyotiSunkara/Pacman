0374b776f6b0c3593e23010547b3b9687354eb5d
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-08-25 12:59:51 +0530

    Cell structure defined

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,60 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <learnopengl/shaders.h>
 
 #include <iostream>
 using namespace std;
 #include "cell.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void reshape(GLFWwindow* window, int w, int h);
 void processInput(GLFWwindow *window);
 
 // Settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 500;
+const unsigned int SCR_HEIGHT = 500;
+static Cell *cell;          // Maze
+static int width, height;   // Maze dimensions
+static double R, G, B;		// Background colour
 
 int main(int argc, char ** argv) {
 
     if (argc > 1) {
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-                cout << "usage: " << argv[0] << " [--auto]" << endl;
+                cout << "Usage: " << argv[0] << " [--auto]" << endl;
                 return 0;
         }
     }
+
+    while(true) {
+		cout << "Please input the width of maze (5 ~ 50)" << endl;
+		cin >> width;
+		if(width > 50 || width < 5)	cout << "Out of range!" << endl;
+		else break;
+	}
+
+	while(true) {
+		cout << "Please input the height of maze (5 ~ 50)" << endl;
+		cin >> height;
+		if(height > 50 || height < 5)	cout << "Out of range!" << endl;
+		else break;
+	}
+
+    // User Manual message 
+	cout << endl;
+	cout << "Space bar : Generate maze" << endl;
+
+    cell = new Cell[width * height];
+
+	srand((unsigned)time(NULL));
+    // Background color 
+	R = (rand() % 256) / 255.0;
+	G = (rand() % 256) / 255.0;
+	B = (rand() % 256) / 255.0;
 
 
     // GLFW: initialize and configure
@@ -31,23 +64,22 @@ int main(int argc, char ** argv) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
-    // glfw window creation
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    // GLFW window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Pacman", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+    glfwSetWindowPos(window, 100, 100);
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, reshape);
 
     // glad: load all OpenGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-    
 
     // build and compile our shader program
     Shader ourShader("../src/shader.vert", "../src/shader.frag"); // you can name your shader files however you like
@@ -81,17 +113,26 @@ int main(int argc, char ** argv) {
     // glBindVertexArray(0);
 
 
-    // render loop
+    // Render loop
     while (!glfwWindowShouldClose(window)) {
-        // input
+        // Input
         processInput(window);
 
-        // render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // Background
+        glClearColor(R, G, B, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // render the triangle
+        // Transformations
+        glm::mat4 transform = glm::mat4(1.0f); // Identity matrix first
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+
+        // Get matrix's uniform location and set matrix
         ourShader.use();
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // Render the triangle
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -100,7 +141,7 @@ int main(int argc, char ** argv) {
         glfwPollEvents();
     }
 
-    // Optional: de-allocate all resources once they've outlived their purpose:
+    // Optional: De-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
@@ -109,18 +150,23 @@ int main(int argc, char ** argv) {
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-void processInput(GLFWwindow *window)
-{
+// Process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
 }
 
 // GLFW: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+void reshape(GLFWwindow* window, int w, int h) {
+    // Make sure the viewport matches the new window dimensions; note that w and 
+    // h will be significantly larger than specified on retina displays.
+    glViewport(0, 0, w, h);
+    return;
 }
 
